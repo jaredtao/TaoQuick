6c0fafa6502c0719ef1fa4ca37fa619e2f0c41c7
jared <jared2020@163.com>
2019-07-18 11:01:07 +0800

    view center in screen

--- TaoQuickDemo/Src/main.cpp
+++ TaoQuickDemo/Src/main.cpp
@@ -6,7 +6,7 @@
 #include <QQmlEngine>
 #include <QGradient>
 #include <QDebug>
-
+#include <QScreen>
 int main(int argc, char **argv)
 {
     QGuiApplication app(argc, argv);
@@ -21,6 +21,11 @@ int main(int argc, char **argv)
     TaoView view;
     view.rootContext()->setContextProperty("view", &view);
     view.setSource(QUrl(QStringLiteral("qrc:/Qml/main.qml")));
+    //View center in screen
+    auto screenGeo = app.primaryScreen()->geometry();
+    auto viewGeo = view.geometry();
+    QPoint centerPos = {(screenGeo.width() - viewGeo.width()) / 2, (screenGeo.height() - viewGeo.height()) /2 };
+    view.setPosition(centerPos);
     view.show();
 
     return app.exec();
