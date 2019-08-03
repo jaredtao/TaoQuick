070420123cc580cf51d621bd5765c94b707f80fe
jaredtao <wentaojia2014@163.com>
2019-08-03 18:03:49 +0800

    update script

--- src/TaoQuick/Qml/Updateqmldir.ps1
+++ src/TaoQuick/Qml/Updateqmldir.ps1
@@ -1,1 +1,15 @@
-Get-ChildItem BasicComponent
+$cur= Get-Location
+$ver="1.0"
+if (Test-Path qmldir) {
+    Clear-Content qmldir
+}
+Get-ChildItem -Recurse BasicComponent | ForEach-Object -Process {
+    if ($_ -is [System.IO.FileInfo]) {
+        $filePath = Resolve-Path -Relative $_.Directory
+        $filePath = $filePath.Remove(0, 2)
+        $filePath = $filePath + "\" + $_.Name
+        $filePath=$filePath.Replace('\', '/')
+        $line = ($_.BaseName, $ver, $filePath) -join " "
+        $line | Out-File -Append -Encoding "utf8" qmldir
+    }
+}
