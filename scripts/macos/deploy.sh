1f9db0ebba55a9f2e2b2154f0b2fa86989b9aa50
jaredtao <wentaojia2014@163.com>
2019-08-03 23:42:31 +0800

    update ci script

--- scripts/macos/deploy.sh
+++ scripts/macos/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-mkdir bin/release/TaoQuickDemo.app/Contents/Resources/qml
-cp -R bin/release/TaoQuick bin/release/TaoQuickDemo.app/Contents/Resources/qml/TaoQuick
-/usr/local/opt/qt/bin/macdeployqt bin/release/TaoQuickDemo.app -qmldir=/usr/local/opt/qt/qml -verbose=1 -dmg
-mv bin/release/TaoQuickDemo.dmg bin/release/TaoQuickDemo_macos10-14_xcode10-2.dmg
+mkdir bin/release/TaoQuickApp.app/Contents/Resources/qml
+cp -R bin/release/TaoQuick bin/release/TaoQuickApp.app/Contents/Resources/qml/TaoQuick
+/usr/local/opt/qt/bin/macdeployqt bin/release/TaoQuickApp.app -qmldir=/usr/local/opt/qt/qml -verbose=1 -dmg
+mv bin/release/TaoQuickApp.dmg bin/release/TaoQuickApp_macos10-14_xcode10-2.dmg
