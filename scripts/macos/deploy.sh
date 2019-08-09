02c805341c0b66c3bb0085de5ed83088c9ed48ca
jared <jared2020@163.com>
2019-08-09 22:34:48 +0800

    missing png

--- scripts/macos/deploy.sh
+++ scripts/macos/deploy.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 mkdir bin/release/TaoQuickApp.app/Contents/Resources/qml
-#cp -R bin/release/TaoQuick bin/release/TaoQuickApp.app/Contents/Resources/qml/TaoQuick
 /usr/local/opt/qt/bin/macdeployqt bin/release/TaoQuickApp.app -qmldir=/usr/local/opt/qt/qml -verbose=1 -dmg
 mv bin/release/TaoQuickApp.dmg bin/release/TaoQuickApp_macos10-14_xcode10-2.dmg
