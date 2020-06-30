a814f27d80d7a88949d0e68ac5e3863024cf7310
jaredtao <jared2020@163.com>
2020-07-01 00:16:13 +0800

    move to contents

--- examples/TaoQuickShow/Src/main.cpp
+++ examples/TaoQuickShow/Src/main.cpp
@@ -1,29 +1,51 @@
 #include "TaoView.h"
+#include "Trans.h"
 #include "logger.h"
 #include <QGuiApplication>
 #include <QQmlContext>
 #include <QQmlEngine>
-#include "Trans.h"
-#include "PluginLoader.h"
-
-int main(int argc, char **argv)
+#include <QUrl>
+#include <QDir>
+static void prepareApp()
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setOrganizationName("JaredTao");
+    QCoreApplication::setOrganizationDomain("https://JaredTao.gitee.io");
+    QCoreApplication::setApplicationName("TaoQuickApp");
+}
+static void beforeUiReady(QQmlContext *ctx)
+{
+    Q_UNUSED(ctx)
+}
+static void afterUiReady()
+{
 
+}
+int main(int argc, char** argv)
+{
+    prepareApp();
     QGuiApplication app(argc, argv);
-    app.setOrganizationName("JaredTao");
-    app.setOrganizationDomain("https://jaredtao.github.io");
+
     Logger::initLog();
     Trans trans;
-    trans.loadFolder(app.applicationDirPath() + "/Trans");
-    PluginLoader loader;
+
     TaoView view;
 
+    beforeUiReady(view.rootContext());
+    view.engine()->addImportPath(qmlPath);
+    view.rootContext()->setContextProperty("qmlPath", qmlPath);
+    view.rootContext()->setContextProperty("imgPath", imgPath);
+    view.rootContext()->setContextProperty("appPath", app.applicationDirPath());
     view.rootContext()->setContextProperty("view", &view);
     view.rootContext()->setContextProperty("trans", &trans);
-    view.rootContext()->setContextProperty("pluginLoader", &loader);
 
-    view.setSource(QUrl(QStringLiteral("qrc:/Qml/main.qml")));
+    const QUrl url(qmlPath + QStringLiteral("main.qml"));
+    QObject::connect(&view, &QQuickView::statusChanged, [=](QQuickView::Status status){
+        if (status == QQuickView::Status::Ready) {
+            afterUiReady();
+        }
+    });
+    view.setSource(url);
     view.moveToScreenCenter();
     view.show();
 
