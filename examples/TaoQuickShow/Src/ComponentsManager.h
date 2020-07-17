e5a9157a10727ca0d6b5339757799cfa44e9c05a
jaredtao <jared2020@163.com>
2020-07-18 02:32:54 +0800

    update path

--- examples/TaoQuickShow/Src/ComponentsManager.h
+++ examples/TaoQuickShow/Src/ComponentsManager.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#include <QObject>
-#include <QString>
 #include "TaoObject.h"
 #include <QJsonArray>
-class ComponentsMgr : public QObject, public TaoObject
-{
+#include <QObject>
+#include <QString>
+class ComponentsMgr : public QObject, public TaoObject {
     Q_OBJECT
     Q_PROPERTY(QJsonArray comps READ comps WRITE setComps NOTIFY compsChanged)
 public:
-    explicit ComponentsMgr(QObject *parent = nullptr);
-    Q_INVOKABLE void loadFolder(const QString &folder);
+    explicit ComponentsMgr(QObject* parent = nullptr);
+    Q_INVOKABLE void loadFolder(const QString& folder);
+
 public:
     void init() override;
 
@@ -20,18 +20,15 @@ public:
 
     void afterUiReady() override;
 
-    const QJsonArray &comps() const;
+    const QJsonArray& comps() const;
 
 public slots:
-    void setComps(const QJsonArray &comps);
+    void setComps(const QJsonArray& comps);
 
 signals:
     void compsChanged(QJsonArray comps);
 
 protected:
-
 private:
-
     QJsonArray m_comps;
 };
-
