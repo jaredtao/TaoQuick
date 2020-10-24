dbefa2b2dd82561c2bbb7a0962851357770e5110
jaredtao <jared2020@163.com>
2020-10-24 12:11:53 +0800

    update format

--- examples/TaoQuickShow/Src/stdafx.h
+++ examples/TaoQuickShow/Src/stdafx.h
@@ -1,32 +1,30 @@
 #pragma once
 /* Add C includes here */
 
-
 /* Add C++ includes here */
 #if defined __cplusplus
-#include <QtGui>
-#include <QtCore>
-#include <QString>
-#include <QObject>
-#include <QHash>
-#include <QMap>
-#include <QFile>
-#include <QDir>
-#include <QBuffer>
-#include <QTime>
-#include <QTimer>
-#include <QMutex>
-#include <QByteArray>
-#include <QThread>
-#include <QDateTime>
-#include <QTextStream>
-#include <QDebug>
+#    include <QtGui>
+#    include <QtCore>
+#    include <QString>
+#    include <QObject>
+#    include <QHash>
+#    include <QMap>
+#    include <QFile>
+#    include <QDir>
+#    include <QBuffer>
+#    include <QTime>
+#    include <QTimer>
+#    include <QMutex>
+#    include <QByteArray>
+#    include <QThread>
+#    include <QDateTime>
+#    include <QTextStream>
+#    include <QDebug>
 
-#include <QtMath>
-#include <QPixmap>
-#include <QImage>
-#include <QPainter>
-#include <QSettings>
+#    include <QtMath>
+#    include <QPixmap>
+#    include <QImage>
+#    include <QPainter>
+#    include <QSettings>
 
 #endif
-
