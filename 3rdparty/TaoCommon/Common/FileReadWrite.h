8d4479cc1c7f23dd49b200cc3a64bcbd3286ee9d
jared <jared2020@163.com>
2020-10-29 15:26:24 +0800

    add Files

---
+++ 3rdparty/TaoCommon/Common/FileReadWrite.h
@@ -0,0 +1,98 @@
+#pragma once
+#include <QDebug>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QString>
+#include "Logger/Logger.h"
+namespace TaoCommon {
+static bool readFile(const QString &filePath, QByteArray &content)
+{
+    QFile file(filePath);
+    if (!file.open(QFile::ReadOnly)) {
+        LOG_WARN << "open file " << filePath << "failed:" << file.errorString();
+        return false;
+    }
+    content = file.readAll();
+    file.close();
+    return true;
+}
+static bool readJson(const QByteArray &data, QJsonDocument &doc)
+{
+    QJsonParseError err;
+    doc = QJsonDocument::fromJson(data, &err);
+    if (doc.isNull()) {
+        LOG_WARN << "parse json failed:" << err.errorString();
+        return false;
+    }
+    return true;
+}
+static bool readJson(const QByteArray &data, QJsonArray &array)
+{
+    QJsonDocument doc;
+    bool ok = readJson(data, doc);
+    if (ok) {
+        array = doc.array();
+    }
+    return ok;
+}
+static bool readJson(const QByteArray &data, QJsonObject &object)
+{
+    QJsonDocument doc;
+    bool ok = readJson(data, doc);
+    if (ok) {
+        object = doc.object();
+    }
+    return ok;
+}
+
+static bool readJsonFile(const QString &filePath, QJsonDocument &jsonDoc)
+{
+    QByteArray data;
+    if (!readFile(filePath, data)) {
+        return false;
+    }
+    return readJson(data, jsonDoc);
+}
+static bool readJsonFile(const QString &filePath, QJsonObject &jsonObj)
+{
+    QByteArray data;
+    if (!readFile(filePath, data)) {
+        return false;
+    }
+    return readJson(data, jsonObj);
+}
+static bool readJsonFile(const QString &filePath, QJsonArray &jsonArray)
+{
+    QByteArray data;
+    if (!readFile(filePath, data)) {
+        return false;
+    }
+    return readJson(data, jsonArray);
+}
+static bool writeFile(const QString &filePath, const QByteArray &content)
+{
+    QFile file(filePath);
+    if (!file.open(QFile::WriteOnly)) {
+        LOG_WARN << "open file " << filePath << "failed:" << file.errorString();
+        return false;
+    }
+    file.write(content);
+    file.close();
+    return true;
+}
+static bool writeJsonFile(const QString &filePath, const QJsonDocument &doc)
+{
+    return writeFile(filePath, doc.toJson());
+}
+static bool writeJsonFile(const QString &filePath, const QJsonArray &jsonArray)
+{
+    return writeJsonFile(filePath, QJsonDocument(jsonArray));
+}
+static bool writeJsonFile(const QString &filePath, const QJsonObject &jsonObj)
+{
+    return writeJsonFile(filePath, QJsonDocument(jsonObj));
+}
+
+} // namespace TaoCommon
---
+++ 3rdparty/TaoCommon/Trans/Trans.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <QObject>
+#include <QHash>
+#include <QList>
+#include <QString>
+#include <QTranslator>
+#include "TaoCommonGlobal.h"
+class QQmlContext;
+class Trans : public QTranslator
+{
+    Q_OBJECT
+    Q_PROPERTY(QString currentLang READ currentLang WRITE setCurrentLang NOTIFY currentLangChanged)
+    Q_PROPERTY(QStringList languages READ languages NOTIFY languagesChanged)
+    Q_PROPERTY(QString transString READ transString NOTIFY transStringChanged)
+public:
+    Trans(QObject *parent = nullptr);
+
+    Q_INVOKABLE void loadFolder(const QString &folder);
+
+    Q_INVOKABLE bool load(QString &lang, const QString &filePath);
+
+public:
+    void beforeUiReady(QQmlContext *ctx);
+
+    void afterUiReady();
+
+    QString translate(const char *context, const char *sourceText,
+                      const char *disambiguation = nullptr, int n = -1) const override;
+
+public:
+    const QString &currentLang() const;
+
+    const QStringList &languages() const;
+
+    const QString &transString() const;
+
+public slots:
+    QString trans(const QString &source) const;
+    void setCurrentLang(const QString &currentLang);
+signals:
+    void currentLangChanged(const QString &currentLang);
+
+    void languagesChanged(const QStringList &languages);
+
+    void langLoaded(const QString &lang);
+
+    void folderLoaded(const QString &folder);
+
+    void transStringChanged();
+
+protected:
+    void setLanguages(const QStringList &languages);
+
+    void initEnglish();
+
+private:
+    QString m_currentLang;
+    // <"English", <"key", "value">>
+    QHash<QString, QHash<QString, QString>> m_map;
+    QStringList m_languages;
+    QString m_transString;
+    QQmlContext *m_ctx;
+};
