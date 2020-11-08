d9f768a35e98e6b6f43b3d3b01c5ab48d9f491d0
jared <jared2020@163.com>
2020-11-08 21:53:28 +0800

    update table performance

--- examples/TaoQuickShow/Src/DeviceAddTable/DeviceAddItem.cpp
+++ examples/TaoQuickShow/Src/DeviceAddTable/DeviceAddItem.cpp
@@ -4,3 +4,18 @@ DeviceAddItem::DeviceAddItem(QObject *parent)
     : TaoListItemBase(parent)
 {
 }
+bool DeviceAddItem::match(const QString &key)
+{
+    if (key.isEmpty())
+    {
+        return true;
+    }
+    if (m_name.contains(key, Qt::CaseInsensitive)) {
+        return true;
+    } else if (m_address.contains(key, Qt::CaseInsensitive)) {
+        return true;
+    } else if (m_modelString.contains(key, Qt::CaseInsensitive)){
+        return true;
+    }
+    return false;
+}
