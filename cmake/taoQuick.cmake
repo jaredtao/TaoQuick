3051270baf835b008a4db57852d8aa576d7a3290
jared <jared2020@163.com>
2020-11-09 01:16:08 +0800

    add some cmake files

---
+++ cmake/taoQuick.cmake
@@ -0,0 +1,15 @@
+set(TaoQuickPath ${CMAKE_SOURCE_DIR}/src/TaoQuick/imports)
+
+if (CMAKE_BUILD_TYPE MATCHES "Release")
+    set(TaoQuickRes ${TaoQuickPath}/TaoQuick/TaoQuick.qrc)
+    add_compile_definitions(TaoQuickImportPath="qrc:///")
+    add_compile_definitions(TaoQuickImagePath="qrc:/TaoQuick/Images/")
+else()
+    add_compile_definitions(TaoQuickImportPath="file:///${TaoQuickPath}")
+    add_compile_definitions(TaoQuickImagePath="file:///${TaoQuickPath}/TaoQuick/Images/")
+endif()
+add_compile_definitions(QML_IMPORT_PATH="${TaoQuickPath}")
+add_compile_definitions(QML2_IMPORT_PATH="${TaoQuickPath}")
+add_compile_definitions(QML_DESIGNER_IMPORT_PATH="${TaoQuickPath}")
+
+
