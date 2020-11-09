3f654d69c8866dbdc73de46adfaa9e70e2ea0ec4
jared <jared2020@163.com>
2020-11-10 00:41:40 +0800

    update cmake version str

--- examples/TaoQuickShow/Src/AppInfo.cpp
+++ examples/TaoQuickShow/Src/AppInfo.cpp
@@ -1,16 +1,17 @@
 #include "AppInfo.h"
 #include <QQmlContext>
+#include <QSysInfo>
 #include "Ver-u8.h"
 AppInfo::AppInfo(QObject *parent) : QObject(parent)
 {
     m_appName = VER_PRODUCTNAME_STR;
-//    m_appVersion = TaoVer;
-//    m_latestVersion = TaoVer;
-//    m_buildDateTime = TaoDATETIME;
-//    m_buildRevision = TaoREVISIONSTR;
-//    m_copyRight = VER_LEGALCOPYRIGHT_STR;
-//    m_descript = QString::fromLocal8Bit(VER_FILEDESCRIPTION_STR);
-//    m_compilerVendor = TaoCompilerVendor;
+    m_appVersion = TaoVer;
+    m_latestVersion = TaoVer;
+    m_buildDateTime = TaoDATETIME;
+    m_buildRevision = TaoREVISIONSTR;
+    m_copyRight = VER_LEGALCOPYRIGHT_STR;
+    m_descript = QString::fromLocal8Bit(VER_FILEDESCRIPTION_STR);
+    m_compilerVendor = QString("%1(%2 %3)").arg(QT_VERSION_STR).arg(CXX_COMPILER_ID).arg(QSysInfo::buildCpuArchitecture());
 }
 
 void AppInfo::beforeUiReady(QQmlContext *ctx)
