8f802b9b0b3f1c151957b755d0425b0d898de176
jared <jared2020@163.com>
2020-11-09 14:21:13 +0800

    update cmake

--- cmake/taoQuick.cmake
+++ cmake/taoQuick.cmake
@@ -1,7 +1,7 @@
 set(TaoQuickPath ${CMAKE_SOURCE_DIR}/src/TaoQuick/imports)
 
 if (CMAKE_BUILD_TYPE MATCHES "Release")
-    set(TaoQuickRes ${TaoQuickPath}/TaoQuick/TaoQuick.qrc)
+    set(TaoQuickRes ${TaoQuickPath}/TaoQuick/TaoQuick.qrc CACHE STRING "tao quick res path")
     add_compile_definitions(TaoQuickImportPath="qrc:///")
     add_compile_definitions(TaoQuickImagePath="qrc:/TaoQuick/Images/")
 else()
