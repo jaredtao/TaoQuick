e4a7186889c214bc372a15d1c0a7f026c37d0dac
jared <jared2020@163.com>
2020-11-13 13:35:42 +0800

    update PropertyHeader

--- examples/TaoQuickShow/Src/DeviceAddTable/DeviceAddItem.h
+++ examples/TaoQuickShow/Src/DeviceAddTable/DeviceAddItem.h
@@ -1,89 +1,21 @@
 #pragma once
 
 #include "TaoModel/TaoListItemBase.h"
+#include"Common/PropertyHelper.h"
 #include <QObject>
 class DeviceAddItem : public TaoListItemBase
 {
     Q_OBJECT
-    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
-    Q_PROPERTY(QString address READ address WRITE setAddress NOTIFY addressChanged)
-    Q_PROPERTY(QString modelString READ modelString WRITE setModelString NOTIFY modelStringChanged)
-    Q_PROPERTY(bool online READ online WRITE setOnline NOTIFY onlineChanged)
 
+    AUTO_PROPERTY(QString, name, "")
+    AUTO_PROPERTY(QString, address, "")
+    AUTO_PROPERTY(QString, modelString, "")
+    AUTO_PROPERTY(bool, online, false)
 public:
     explicit DeviceAddItem(QObject *parent = nullptr);
-
-    const QString &name() const
-    {
-        return m_name;
-    }
-
-    const QString &address() const
-    {
-        return m_address;
-    }
-
-    const QString &modelString() const
-    {
-        return m_modelString;
-    }
-
-    bool online() const
-    {
-        return m_online;
-    }
+    virtual ~DeviceAddItem() override;
 
     bool match(const QString &key) override;
-public slots:
-    void setName(const QString &name)
-    {
-        if (m_name == name)
-            return;
-
-        m_name = name;
-        emit nameChanged(m_name);
-    }
-
-    void setAddress(const QString &address)
-    {
-        if (m_address == address)
-            return;
-
-        m_address = address;
-        emit addressChanged(m_address);
-    }
-
-    void setModelString(const QString &modelString)
-    {
-        if (m_modelString == modelString)
-            return;
-
-        m_modelString = modelString;
-        emit modelStringChanged(m_modelString);
-    }
-
-    void setOnline(bool online)
-    {
-        if (m_online == online)
-            return;
-
-        m_online = online;
-        emit onlineChanged(m_online);
-    }
-
-signals:
-
-    void nameChanged(const QString &name);
-    void addressChanged(const QString &address);
-    void modelStringChanged(const QString &modelString);
-    void onlineChanged(bool online);
-
 private:
-    QString m_name;
 
-    QString m_address;
-
-    QString m_modelString;
-
-    bool m_online = false;
 };
