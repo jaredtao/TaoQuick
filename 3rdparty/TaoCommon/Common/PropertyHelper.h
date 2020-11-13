dda6161b1771ead582bd375b5058b49e7c3fa894
jared <jared2020@163.com>
2020-11-13 13:35:42 +0800

    update PropertyHeader

--- 3rdparty/TaoCommon/Common/PropertyHelper.h
+++ 3rdparty/TaoCommon/Common/PropertyHelper.h
@@ -1,47 +1,68 @@
 ﻿#pragma once
 #include <QObject>
+#include <QtGlobal>
 
-#define READ_PROPERTY(TYPE, NAME)                                                                                                                              \
-private:                                                                                                                                                       \
-    Q_PROPERTY(TYPE NAME READ NAME NOTIFY NAME##Changed)                                                                                                       \
-public:                                                                                                                                                        \
-    const TYPE &NAME() const                                                                                                                                   \
-    {                                                                                                                                                          \
-        return m_##NAME;                                                                                                                                       \
-    }                                                                                                                                                          \
-    Q_SIGNAL void NAME##Changed(const TYPE &value);                                                                                                            \
-                                                                                                                                                               \
-private:                                                                                                                                                       \
-    TYPE m_##NAME;
 
-#define READONLY_PROPERTY(TYPE, NAME)                                                                                                                          \
-private:                                                                                                                                                       \
-    Q_PROPERTY(TYPE NAME READ NAME CONSTANT)                                                                                                                   \
-public:                                                                                                                                                        \
-    const TYPE &NAME() const                                                                                                                                   \
-    {                                                                                                                                                          \
-        return m_##NAME;                                                                                                                                       \
-    }                                                                                                                                                          \
-                                                                                                                                                               \
-private:                                                                                                                                                       \
-    TYPE m_##NAME;
+template <typename T>
+struct Compare;
 
-#define AUTO_PROPERTY(TYPE, NAME)                                                                                                                              \
-private:                                                                                                                                                       \
-    Q_PROPERTY(TYPE NAME READ NAME WRITE NAME NOTIFY NAME##Changed)                                                                                            \
-public:                                                                                                                                                        \
-    const TYPE &NAME() const                                                                                                                                   \
-    {                                                                                                                                                          \
-        return m_##NAME;                                                                                                                                       \
-    }                                                                                                                                                          \
-    void NAME(const TYPE &value)                                                                                                                               \
-    {                                                                                                                                                          \
-        if (m_##NAME == value)                                                                                                                                 \
-            return;                                                                                                                                            \
-        m_##NAME = value;                                                                                                                                      \
-        emit NAME##Changed(value);                                                                                                                             \
-    }                                                                                                                                                          \
-    Q_SIGNAL void NAME##Changed(const TYPE &value);                                                                                                            \
-                                                                                                                                                               \
-private:                                                                                                                                                       \
-    TYPE m_##NAME;
+template <typename T>
+struct Compare
+{
+    static bool isEqual(const T &t1, const T &t2)
+    {
+        return t1 == t2;
+    }
+};
+template<>
+struct Compare<float>
+{
+    static bool isEqual(float f1, float f2)
+    {
+        return qFuzzyCompare(f1, f2);
+    }
+};
+template<>
+struct Compare<double>
+{
+    static bool isEqual(double d1, double d2)
+    {
+        return qFuzzyCompare(d1, d2);
+    }
+};
+
+#define READ_PROPERTY(T, NAME, InitValue)                                                          \
+private:                                                                                           \
+    Q_PROPERTY(T NAME READ NAME NOTIFY NAME##Changed)                                              \
+public:                                                                                            \
+    const T &NAME() const { return m_##NAME; }                                                     \
+    Q_SIGNAL void NAME##Changed(const T &value);                                                   \
+                                                                                                   \
+private:                                                                                           \
+    T m_##NAME = InitValue;
+
+#define READONLY_PROPERTY(T, NAME, InitValue)                                                      \
+private:                                                                                           \
+    Q_PROPERTY(T NAME READ NAME CONSTANT)                                                          \
+public:                                                                                            \
+    const T &NAME() const { return m_##NAME; }                                                     \
+                                                                                                   \
+private:                                                                                           \
+    T m_##NAME = InitValue;
+
+#define AUTO_PROPERTY(T, NAME, InitValue)                                                          \
+private:                                                                                           \
+    Q_PROPERTY(T NAME READ NAME WRITE set_##NAME NOTIFY NAME##Changed)                             \
+public:                                                                                            \
+    const T &NAME() const { return m_##NAME; }                                                     \
+    Q_SLOT void set_##NAME(const T &value)                                                         \
+    {                                                                                              \
+        if (Compare<T>::isEqual(m_##NAME, value))                                                  \
+            return;                                                                                \
+        m_##NAME = value;                                                                          \
+        emit NAME##Changed(value);                                                                 \
+    }                                                                                              \
+    Q_SIGNAL void NAME##Changed(const T &value);                                                   \
+                                                                                                   \
+private:                                                                                           \
+    T m_##NAME = InitValue;
