a2edf86ca9a13b7f7317c721d2a4243d4ce1a572
jared <jared2020@163.com>
2020-11-22 00:55:53 +0800

    增加 入门指南.md

---
+++ examples/HelloTaoQuick1/HelloTaoQuick1.pro
@@ -0,0 +1,9 @@
+TEMPLATE = app
+
+QT += gui quick qml quickcontrols2
+
+SOURCES += main.cpp
+
+RESOURCES += Res.qrc
+
+include(../../src/TaoQuick/TaoQuick.pri)
---
+++ examples/HelloTaoQuick2/main.cpp
@@ -0,0 +1,22 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.addImportPath(TaoQuickImportPath);
+    engine.rootContext()->setContextProperty("taoQuickImagePath", TaoQuickImagePath);
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
