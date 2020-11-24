ccbb27a982cfd41188ea67ad594784401b71db31
jared <jared2020@163.com>
2020-11-24 13:14:04 +0800

    更新排序算法

--- examples/TaoQuickShow/Src/DeviceAddTable/DeviceAddModel.h
+++ examples/TaoQuickShow/Src/DeviceAddTable/DeviceAddModel.h
@@ -10,6 +10,7 @@ class DeviceAddModel : public TaoListModel
 public:
     explicit DeviceAddModel(QObject *parent = nullptr);
     virtual ~DeviceAddModel() override;
+    Q_INVOKABLE virtual void sortByRole() override;
 public slots:
     void doUpdateName(int row, const QString &name);
 
@@ -27,6 +28,10 @@ public slots:
     void removeChecked();
     void removeRow(int row);
 private:
+    void sortByName(Qt::SortOrder order);
+    void sortByAddress(Qt::SortOrder order);
+    void sortByModel(Qt::SortOrder order);
+
     DeviceAddItem *genOne(uint32_t value);
 private:
     DeviceAddModelPrivate *d;
