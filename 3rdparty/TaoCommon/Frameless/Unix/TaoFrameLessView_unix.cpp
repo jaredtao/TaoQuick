8d1b505c7474b8ac2619e901f8ddd9a417559af3
jared <jared2020@163.com>
2020-12-26 23:00:30 +0800

    update FrameLess

--- 3rdparty/TaoCommon/Frameless/Unix/TaoFrameLessView_unix.cpp
+++ 3rdparty/TaoCommon/Frameless/Unix/TaoFrameLessView_unix.cpp
@@ -1,83 +1,19 @@
-#include "TaoFrameLessView.h"
-
+#include "Frameless/TaoFrameLessView.h"
 #include <QGuiApplication>
 #include <QQuickItem>
 #include <QScreen>
 #include <QWindow>
-#if WIN32
 
-#    include <WinUser.h>
-#    include <dwmapi.h>
-#    include <objidl.h> // Fixes error C2504: 'IUnknown' : base class undefined
-#    include <windows.h>
-#    include <windowsx.h>
-#    pragma comment(lib, "Dwmapi.lib") // Adds missing library, fixes error LNK2019: unresolved
-                                       // external symbol __imp__DwmExtendFrameIntoClientArea
-#    pragma comment(lib, "user32.lib")
-
-#endif
-
-#ifdef WIN32
-static long hitTest(RECT winrect, long x, long y, int borderWidth)
+class TaoFrameLessViewPrivate
 {
-    // 鼠标区域位于窗体边框，进行缩放
-    if ((x >= winrect.left) && (x < winrect.left + borderWidth) && (y >= winrect.top)
-        && (y < winrect.top + borderWidth)) {
-        return HTTOPLEFT;
-    } else if (x < winrect.right && x >= winrect.right - borderWidth && y >= winrect.top
-               && y < winrect.top + borderWidth) {
-        return HTTOPRIGHT;
-    } else if (x >= winrect.left && x < winrect.left + borderWidth && y < winrect.bottom
-               && y >= winrect.bottom - borderWidth) {
-        return HTBOTTOMLEFT;
-    } else if (x < winrect.right && x >= winrect.right - borderWidth && y < winrect.bottom
-               && y >= winrect.bottom - borderWidth) {
-        return HTBOTTOMRIGHT;
-    } else if (x >= winrect.left && x < winrect.left + borderWidth) {
-        return HTLEFT;
-    } else if (x < winrect.right && x >= winrect.right - borderWidth) {
-        return HTRIGHT;
-    } else if (y >= winrect.top && y < winrect.top + borderWidth) {
-        return HTTOP;
-    } else if (y < winrect.bottom && y >= winrect.bottom - borderWidth) {
-        return HTBOTTOM;
-    } else {
-        return 0;
-    }
-}
-#endif
-
-static bool isMaxWin(QWindow *win)
+public:
+    bool m_isMax;
+    QQuickItem *m_titleItem = nullptr;
+};
+TaoFrameLessView::TaoFrameLessView(QWindow *parent) : Super(parent), d(new TaoFrameLessViewPrivate) { }
+TaoFrameLessView::~TaoFrameLessView()
 {
-    return win->windowState() == Qt::WindowMaximized;
-}
-static bool isFullWin(QQuickView *win)
-{
-    return win->windowState() == Qt::WindowFullScreen;
-}
-
-TaoFrameLessView::TaoFrameLessView(QWindow *parent) : QQuickView(parent)
-{
-    setFlags(Qt::CustomizeWindowHint | Qt::Window | Qt::FramelessWindowHint
-             | Qt::WindowMinMaxButtonsHint | Qt::WindowTitleHint | Qt::WindowSystemMenuHint);
-    setResizeMode(SizeRootObjectToView);
-
-    // WS_THICKFRAME 带回Areo效果
-#if WIN32
-    DWORD style = ::GetWindowLong(HWND(winId()), GWL_STYLE);
-    style |= WS_THICKFRAME;
-    ::SetWindowLong(HWND(winId()), GWL_STYLE, style);
-#endif
-
-    setIsMax(isMaxWin(this));
-    connect(this, &QWindow::windowStateChanged, this,
-            [&](Qt::WindowState state) { setIsMax(state == Qt::WindowMaximized); });
-}
-
-TaoFrameLessView::~TaoFrameLessView() { }
-void TaoFrameLessView::setTitleItem(QQuickItem *item)
-{
-    m_titleItem = item;
+    delete d;
 }
 QRect TaoFrameLessView::calcCenterGeo(const QRect &screenGeo, const QSize &normalSize)
 {
@@ -105,83 +41,23 @@ void TaoFrameLessView::moveToScreenCenter()
     setGeometry(geo);
     update();
 }
-
+bool TaoFrameLessView::isMax() const
+{
+    return d->m_isMax;
+}
+QQuickItem *TaoFrameLessView::titleItem() const
+{
+    return d->m_titleItem;
+}
 void TaoFrameLessView::setIsMax(bool isMax)
 {
-    if (m_isMax == isMax)
+    if (d->m_isMax == isMax)
         return;
 
-    m_isMax = isMax;
-    emit isMaxChanged(m_isMax);
+    d->m_isMax = isMax;
+    emit isMaxChanged(d->m_isMax);
 }
-
-#if WIN32
-const long border_width = 6;
-#    if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-bool TaoFrameLessView::nativeEvent(const QByteArray &eventType, void *message, qintptr *result)
-#    else
-bool TaoFrameLessView::nativeEvent(const QByteArray &eventType, void *message, long *result)
-#    endif
+void TaoFrameLessView::setTitleItem(QQuickItem *item)
 {
-    if (!result) {
-        return false;
-    }
-    MSG *msg = reinterpret_cast<MSG *>(message);
-    if (!msg || !msg->hwnd) {
-        return false;
-    }
-    switch (msg->message) {
-    case WM_NCCALCSIZE: {
-        const auto mode = static_cast<BOOL>(msg->wParam);
-        *result = mode ? WVR_REDRAW : 0;
-
-        const auto clientRect = mode
-                ? &(reinterpret_cast<LPNCCALCSIZE_PARAMS>(msg->lParam)->rgrc[0])
-                : reinterpret_cast<LPRECT>(msg->lParam);
-        //规避 拖动border进行resize时界面闪烁
-        if (!isMaxWin(this) && !isFullWin(this)) {
-            if (clientRect->top != 0) {
-                clientRect->top -= 0.1;
-            }
-        } else {
-            if (clientRect->top != 0) {
-                clientRect->top += 0.1;
-            }
-        }
-        return true;
-    }
-    case WM_NCHITTEST: {
-        *result = 0;
-
-        RECT winrect;
-        GetWindowRect(HWND(winId()), &winrect);
-
-        long x = GET_X_LPARAM(msg->lParam);
-        long y = GET_Y_LPARAM(msg->lParam);
-
-        *result = 0;
-        if (!isMaxWin(this) && !isFullWin(this)) { //非最大化、非全屏时，进行命中测试，处理边框拖拽
-            *result = hitTest(winrect, x, y, border_width);
-            if (0 != *result) {
-                return true;
-            }
-        }
-
-        if (m_titleItem) {
-            auto titlePos = m_titleItem->mapToGlobal(m_titleItem->position());
-            titlePos = mapFromGlobal(titlePos.toPoint());
-            auto titleRect =
-                    QRect(titlePos.x(), titlePos.y(), m_titleItem->width(), m_titleItem->height());
-            double dpr = qApp->devicePixelRatio();
-            QPoint pos = mapFromGlobal(QPoint(x / dpr, y / dpr));
-            if (titleRect.contains(pos)) {
-                *result = HTCAPTION;
-                return true;
-            }
-        }
-        return false;
-    } // end case WM_NCHITTEST
-    }
-    return QQuickView::nativeEvent(eventType, message, result);
+    d->m_titleItem = item;
 }
-#endif
--- 3rdparty/TaoCommon/Frameless/Win/WindowsContext.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include <QtCore/qt_windows.h>
