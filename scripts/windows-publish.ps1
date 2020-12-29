7990e810994e4718c183f14ff5972ad64f119c13
jared <jared2020@163.com>
2020-12-29 23:02:05 +0800

    update windows package

--- scripts/windows-publish.ps1
+++ scripts/windows-publish.ps1
@@ -2,7 +2,14 @@
 param (
     [string] $archiveName, [string] $targetName
 )
-
+# 外部环境变量包括:
+# VCINSTALLDIR: 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC'
+# archiveName: ${{ matrix.qt_ver }}-${{ matrix.qt_arch }}
+# winSdkDir: ${{ steps.build.outputs.winSdkDir }}
+# winSdkVer: ${{ steps.build.outputs.winSdkVer }}
+# vcToolsInstallDir: ${{ steps.build.outputs.vcToolsInstallDir }}
+# vcToolsRedistDir: ${{ steps.build.outputs.vcToolsRedistDir }}
+# msvcArch: ${{ matrix.msvc_arch }}
 
 $scriptDir = $PSScriptRoot
 $currentDir = Get-Location
@@ -20,6 +27,8 @@ function Main() {
     # 删除不必要的文件
     $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
     Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
+    Copy-Item "$env:vcToolsRedistDir\onecore\$env:msvcArch\*.CRT\*.dll" $archiveName\
+    Copy-Item "$env:winSdkDir\Redist\${env:winSdkVer}ucrt\DLLs\$env:msvcArch\*.dll" $archiveName\
     # 打包zip
     Compress-Archive -Path $archiveName $archiveName'.zip'
 }
