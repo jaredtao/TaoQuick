b4b57a619b0c714bb92f1f89901ac4cb413b34c2
jared <jared2020@163.com>
2021-01-02 15:02:16 +0800

    trim path

--- scripts/windows-publish.ps1
+++ scripts/windows-publish.ps1
@@ -37,10 +37,10 @@ function Main() {
     $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
     Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
     # 拷贝vcRedist dll
-    $redistDll="{0}onecore\{1}\*.CRT\*.dll" -f $env:vcToolsRedistDir,$env:msvcArch
+    $redistDll="{0}onecore\{1}\*.CRT\*.dll" -f $env:vcToolsRedistDir.Trim(),$env:msvcArch
     Copy-Item $redistDll $archiveName\
     # 拷贝WinSDK dll
-    $sdkDll="{0}Redist\{1}ucrt\DLLs\{2}\*.dll" -f $env:winSdkDir,$env:winSdkVer,$env:msvcArch
+    $sdkDll="{0}Redist\{1}ucrt\DLLs\{2}\*.dll" -f $env:winSdkDir.Trim(),$env:winSdkVer,$env:msvcArch
     Copy-Item $sdkDll $archiveName\
     # 打包zip
     Compress-Archive -Path $archiveName $archiveName'.zip'
