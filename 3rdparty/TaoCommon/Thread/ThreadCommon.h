84978f97bccade5626f7aab5cb4abe0c1f6e3f4c
jared <jared2020@163.com>
2021-01-25 14:03:56 +0800

    remove TaoCommon files

--- 3rdparty/TaoCommon/Thread/ThreadCommon.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <QMetaType>
-#include <functional>
-namespace TaoCommon {
-using WorkCallback = std::function<bool()>;
-using WorkResultCallback = std::function<void(bool)>;
-}
-Q_DECLARE_METATYPE(TaoCommon::WorkCallback);
-Q_DECLARE_METATYPE(TaoCommon::WorkResultCallback);
