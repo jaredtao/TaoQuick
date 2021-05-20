c498982b18c812d174d84771cb70c0986c58673a
jared <jared2020@163.com>
2021-05-20 09:50:56 +0800

    add QuickTree

---
+++ 3rdparty/TaoCommon/src/TaoCommon/QuickTree/Model/QuickTreeItem.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <QObject>
+#include <QVariant>
+#include <QVector>
+#include "BizCommon_global.h"
+class BIZCOMMON_EXPORT QuickTreeItem : public QObject
+{
+    Q_OBJECT
+public:
+    explicit QuickTreeItem(QuickTreeItem *parentItem = nullptr)
+        : m_parentItem(parentItem)
+    {
+    }
+    explicit QuickTreeItem(const QVector<QVariant> &data, QuickTreeItem *parentItem = nullptr)
+        : m_itemData(data)
+        , m_parentItem(parentItem)
+    {
+    }
+
+    virtual ~QuickTreeItem()
+    {
+        qDeleteAll(m_childItems);
+    }
+
+    void appendChild(QuickTreeItem *child)
+    {
+        m_childItems.append(child);
+    }
+	void prependChild(QuickTreeItem *child)
+	{
+        m_childItems.prepend(child);
+	}
+    void appendChildren(const QVector<QuickTreeItem *> &children)
+    {
+        m_childItems.append(children);
+    }
+    void insert(int row, const QVector<QuickTreeItem *> &children)
+    {
+        if (0 <= row && 0 < m_childItems.size())
+        {
+            for (int j = 0; j < children.size(); ++j)
+            {
+                m_childItems.insert(row + j, children[j]);
+            }
+        }
+    }
+    void insert(int row, QuickTreeItem *child)
+    {
+        if (0 <= row && row < m_childItems.size())
+        {
+            m_childItems.insert(row, child);
+        }
+    }
+    void clear()
+    {
+        qDeleteAll(m_childItems);
+        m_childItems.clear();
+        m_itemData.clear();
+    }
+    void removeAt(int row)
+    {
+        if (0 <= row && row < m_childItems.size())
+        {
+            delete m_childItems.at(row);
+            m_childItems.removeAt(row);
+        }
+    }
+
+    QuickTreeItem *child(int row)
+    {
+        if (row < 0 || row >= m_childItems.size())
+            return nullptr;
+        return m_childItems.at(row);
+    }
+    int childCount() const
+    {
+        return m_childItems.count();
+    }
+    int columnCount() const
+    {
+        return m_itemData.count();
+    }
+
+    QVariant data(int column) const
+    {
+        if (0 <= column && column < m_itemData.size())
+        {
+            return m_itemData.at(column);
+        }
+        return {};
+    }
+    void setDatas(const QVector<QVariant> &datas)
+    {
+        m_itemData = datas;
+    }
+    void setData(int index, const QVariant &data)
+    {
+        if (0 <= index && index < m_itemData.size())
+        {
+            m_itemData[index] = data;
+        }
+    }
+    int row() const
+    {
+        if (m_parentItem)
+            return m_parentItem->m_childItems.indexOf(const_cast<QuickTreeItem *>(this));
+        return 0;
+    }
+
+    QuickTreeItem *parentItem() const
+    {
+        return m_parentItem;
+    }
+
+private:
+    QVector<QuickTreeItem *> m_childItems;
+    QVector<QVariant>        m_itemData;
+    QuickTreeItem *          m_parentItem = nullptr;
+};
---
+++ 3rdparty/TaoCommon/src/TaoCommon/QuickTree/Model/QuickTreeModel.cpp
@@ -0,0 +1,98 @@
+#include "QuickTreeModel.h"
+QuickTreeModel::QuickTreeModel(QObject *parent) :QAbstractItemModel(parent)
+{
+    m_rootItem = new QuickTreeItem(nullptr);
+    QVector<QVariant> datas;
+    datas.append("");
+    datas.append("");
+    datas.append("");
+    datas.append("");
+    m_rootItem->setDatas(datas);
+}
+
+QuickTreeModel::~QuickTreeModel()
+{
+    delete m_rootItem;
+}
+
+QVariant QuickTreeModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    if (role != Qt::DisplayRole)
+        return QVariant();
+
+    QuickTreeItem *item = static_cast<QuickTreeItem*>(index.internalPointer());
+
+    return item->data(index.column());
+}
+
+Qt::ItemFlags QuickTreeModel::flags(const QModelIndex &index) const
+{
+    if (!index.isValid())
+          return Qt::NoItemFlags;
+
+      return QAbstractItemModel::flags(index);
+}
+
+QVariant QuickTreeModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (orientation == Qt::Horizontal && role == Qt::DisplayRole)
+        return m_rootItem->data(section);
+
+    return QVariant();
+}
+
+QModelIndex QuickTreeModel::index(int row, int column, const QModelIndex &parent) const
+{
+    if (!hasIndex(row, column, parent))
+        return QModelIndex();
+
+    QuickTreeItem *parentItem;
+
+    if (!parent.isValid())
+        parentItem = m_rootItem;
+    else
+        parentItem = static_cast<QuickTreeItem*>(parent.internalPointer());
+
+    QuickTreeItem *childItem = parentItem->child(row);
+    if (childItem)
+        return createIndex(row, column, childItem);
+    return QModelIndex();
+}
+
+QModelIndex QuickTreeModel::parent(const QModelIndex &index) const
+{
+    if (!index.isValid())
+        return QModelIndex();
+
+    QuickTreeItem *childItem = static_cast<QuickTreeItem*>(index.internalPointer());
+    QuickTreeItem *parentItem = childItem->parentItem();
+
+    if (parentItem == m_rootItem)
+        return QModelIndex();
+
+    return createIndex(parentItem->row(), 0, parentItem);
+}
+
+int QuickTreeModel::rowCount(const QModelIndex &parent) const
+{
+    QuickTreeItem *parentItem;
+    if (parent.column() > 0)
+        return 0;
+
+    if (!parent.isValid())
+        parentItem = m_rootItem;
+    else
+        parentItem = static_cast<QuickTreeItem*>(parent.internalPointer());
+
+    return parentItem->childCount();
+}
+
+int QuickTreeModel::columnCount(const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return static_cast<QuickTreeItem*>(parent.internalPointer())->columnCount();
+    return m_rootItem->columnCount();
+}
