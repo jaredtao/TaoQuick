3ef0ce1878fc72faec30639722081a9d6f3e9d53
jared <jared2020@163.com>
2022-04-21 09:36:09 +0800

    remove auto-gen file: taoMacro.h

--- examples/TaoQuickShow/taoMacro.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define TaoQuickImport "file:///E:/Dev/Tao/TaoQuick/src"
-#define TaoQuickImage "file:///E:/Dev/Tao/TaoQuick/src/TaoQuick/Images/"
-#define qmlPath "file:///E:/Dev/Tao/TaoQuick/examples/TaoQuickShow/Qml/"
-#define contentsPath "file:///E:/Dev/Tao/TaoQuick/examples/TaoQuickShow/Contents/"
-#define imgPath "file:///E:/Dev/Tao/TaoQuick/examples/TaoQuickShow/Image/"
-#define transDir "E:/Dev/Tao/TaoQuick/examples/TaoQuickShow/Trans/"
