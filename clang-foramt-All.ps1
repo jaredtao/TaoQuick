ed19d59acf0b3c8f34fb045ab7cf2e87470f0677
jared <jared2020@163.com>
2024-07-11 14:52:36 +0800

    update format

--- clang-foramt-All.ps1
+++ clang-foramt-All.ps1
@@ -3,6 +3,8 @@ $currentDir=Get-Location
 Write-Host "currentDir" $currentDir
 Write-Host "scriptDir" $scriptDir
 $names="*.h","*.cpp","*.c","*.hpp"
+$env:Path="C:\Program Files\LLVM\bin;C:\Qt\Tools\QtCreator\bin\clang\bin;$env:Path"
 Get-ChildItem -Include $names -Recurse $currentDir | ForEach-Object {
         clang-format -i $_.FullName
 }
+timeout /t 3
